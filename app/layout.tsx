1eee8f6fcdeb505204a871b0c05ac1641c1b2b5d
anhkiet <kiet47579@gmail.com>
2023-07-18 15:06:42 +0700

    create -list

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import { Nunito } from "next/font/google";
+
+
 import "./globals.css";
 import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
 import Navbar from "./layouts/navbar";
 import ClientOnly from "./components/ClientOnly";
+
 import RegisterModal from "./components/auth/RegisterModal";
-import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/auth/LoginModal";
+import RentModal from "./components/rent/RentModal";
+
+import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata: Metadata = {
@@ -26,11 +31,14 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider/>
+          <RentModal/>
           <LoginModal/>
           <RegisterModal/>
           <Navbar currentUser={currentUser}/>
         </ClientOnly>
+        <div className="pb-20 pt-28">
         {children}
+        </div>
       </body>
     </html>
   );
