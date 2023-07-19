ab2c3bc35a56d66ae59d802d59b569ec041b9971
anhkiet <kiet47579@gmail.com>
2023-07-19 18:54:09 +0700

    push

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,23 +1,24 @@
 import prisma from "../../app/libs/prismadb";
-
 export interface IListingsParams {
   userId?: string;
   guestCount?: number;
   roomCount?: number;
   bathroomCount?: number;
-  locationValue?: string;
   startDate?: string;
   endDate?: string;
+  locationValue?: string;
   category?: string;
 }
 
-export default async function getListings(params: IListingsParams) {
+export default async function getListings(
+  params: IListingsParams
+) {
   try {
     const {
       userId,
-      guestCount,
-      roomCount,
-      bathroomCount,
+      roomCount, 
+      guestCount, 
+      bathroomCount, 
       locationValue,
       startDate,
       endDate,
@@ -25,60 +26,68 @@ export default async function getListings(params: IListingsParams) {
     } = params;
 
     let query: any = {};
+
     if (userId) {
       query.userId = userId;
     }
 
-    if(category){
-        query.category= category
-    }
-    if(guestCount){
-        query.roomCount ={
-            gte:+guestCount
-        };
-    }
-    if(bathroomCount){
-        query.roomCount ={
-            gte:+bathroomCount
-        };
-    }    if(roomCount){
-        query.roomCount ={
-            gte:+roomCount
-        };
+    if (category) {
+      query.category = category;
     }
 
-    if(locationValue){
-        query.locationValue = locationValue
-
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount
+      }
     }
-    if(startDate && endDate ){
-        query.NOT = {
-            reservations :{
-                some:{
-                    OR:[
-                        {
-                            endDate :{gte:startDate},
-                            startDate:{lte:startDate}
-                        },{
-                        startDate:{lte:endDate},
-                        endDate:{gte:endDate}
-                        },
-                    ]
-                }
-            }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount
+      }
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount
+      }
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate }
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate }
+              }
+            ]
+          }
         }
+      }
     }
+
     const listings = await prisma.listing.findMany({
       where: query,
       orderBy: {
-        createdAt: "desc",
-      },
+        createdAt: 'desc'
+      }
     });
 
     const safeListings = listings.map((listing) => ({
       ...listing,
       createdAt: listing.createdAt.toISOString(),
     }));
+
     return safeListings;
   } catch (error: any) {
     throw new Error(error);
