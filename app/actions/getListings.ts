8f01a5f3d672dfb27b5598d0ecc4e4582e82ea04
anhkiet <kiet47579@gmail.com>
2023-07-19 14:58:40 +0700

    properties-screen

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,21 +1,31 @@
-import prisma from '../../app/libs/prismadb'
+import prisma from "../../app/libs/prismadb";
 
+export interface IListingsParams {
+  userId?: string;
+}
 
-export default async function getListings(){
-    try{
-        const listings = await prisma.listing.findMany({
-            orderBy:{
-                createAt:'desc'
-            }
-        })
+export default async function getListings(params: IListingsParams) {
+  try {
+    const { userId } = params;
 
-        const safeListings = listings.map((listing)=>({
-            ...listing,
-            createAt:listing.createAt.toISOString(),
-
-        }))
-        return safeListings
-    }catch(error:any){
-        throw new Error(error)
+    let query: any = {};
+    if (userId) {
+      query.userId = userId;
     }
+
+    const listings = await prisma.listing.findMany({
+      where: query,
+      orderBy: {
+        createAt: "desc",
+      },
+    });
+
+    const safeListings = listings.map((listing) => ({
+      ...listing,
+      createAt: listing.createAt.toISOString(),
+    }));
+    return safeListings;
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
