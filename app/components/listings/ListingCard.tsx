35dea5ed181153f25d92d90d74ad8e567c774a2a
anhkiet <kiet47579@gmail.com>
2023-07-19 17:59:49 +0700

    final

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -66,40 +66,47 @@ const ListingCard: React.FC<ListingCardProps> = ({
   return (
     <div
       onClick={() => router.push(`/listings/${data.id}`)}
-      className="
-    col-span-1 cursor-pointer ground"
+      className="col-span-1 cursor-pointer group"
     >
       <div className="flex flex-col gap-2 w-full">
         <div
-          className="aspect-square
-    w-full
-    relative
-    overflow-hidden
-    rounded-xl"
+          className="
+            aspect-square 
+            w-full 
+            relative 
+            overflow-hidden 
+            rounded-xl
+          "
         >
           <Image
             fill
-            alt="Listing"
-            src={data.imageSrc}
             className="
-            object-cover
-            h-full
-            w-full
-            group-hover:scale-110
-            transition
+              object-cover 
+              h-full 
+              w-full 
+              group-hover:scale-110 
+              transition
             "
+            src={data.imageSrc}
+            alt="Listing"
           />
-          <div className="absolute top-3 right-3">
+          <div
+            className="
+            absolute
+            top-3
+            right-3
+          "
+          >
             <HeartButton listingId={data.id} currentUser={currentUser} />
           </div>
         </div>
         <div className="font-semibold text-lg">
-          {location?.region},{location?.label}
+          {location?.region}, {location?.label}
         </div>
         <div className="font-light text-neutral-500">
           {reservationDate || data.category}
         </div>
-        <div className="flex flex-row items-center-gap-1">
+        <div className="flex flex-row items-center gap-1">
           <div className="font-semibold">$ {price}</div>
           {!reservation && <div className="font-light">night</div>}
         </div>
